59a041fc80ac9dd632643d0dea5fce497014f49d
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-03-14 16:53:27 +0400

    Add project files.

---
+++ LogBook/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LogBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AlternateTutorRbtn = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.GroupNameHolderLbl = new System.Windows.Forms.Label();
+            this.MainTutorLbl = new System.Windows.Forms.Label();
+            this.AlternateTutorLbl = new System.Windows.Forms.Label();
+            this.SubjectLbl = new System.Windows.Forms.Label();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.MainTutorRbtn = new Guna.UI2.WinForms.Guna2CustomRadioButton();
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.SuspendLayout();
+            // 
+            // AlternateTutorRbtn
+            // 
+            this.AlternateTutorRbtn.CheckedState.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.AlternateTutorRbtn.CheckedState.BorderThickness = 0;
+            this.AlternateTutorRbtn.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.AlternateTutorRbtn.CheckedState.InnerColor = System.Drawing.Color.CornflowerBlue;
+            this.AlternateTutorRbtn.CheckedState.Parent = this.AlternateTutorRbtn;
+            this.AlternateTutorRbtn.Location = new System.Drawing.Point(144, 117);
+            this.AlternateTutorRbtn.Name = "AlternateTutorRbtn";
+            this.AlternateTutorRbtn.ShadowDecoration.Parent = this.AlternateTutorRbtn;
+            this.AlternateTutorRbtn.Size = new System.Drawing.Size(20, 20);
+            this.AlternateTutorRbtn.TabIndex = 1;
+            this.AlternateTutorRbtn.UncheckedState.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.AlternateTutorRbtn.UncheckedState.BorderThickness = 2;
+            this.AlternateTutorRbtn.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.AlternateTutorRbtn.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            this.AlternateTutorRbtn.UncheckedState.Parent = this.AlternateTutorRbtn;
+            // 
+            // GroupNameHolderLbl
+            // 
+            this.GroupNameHolderLbl.AutoSize = true;
+            this.GroupNameHolderLbl.Font = new System.Drawing.Font("Malgun Gothic Semilight", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GroupNameHolderLbl.Location = new System.Drawing.Point(12, 40);
+            this.GroupNameHolderLbl.Name = "GroupNameHolderLbl";
+            this.GroupNameHolderLbl.Size = new System.Drawing.Size(454, 37);
+            this.GroupNameHolderLbl.TabIndex = 2;
+            this.GroupNameHolderLbl.Text = "FSDA 3914 Windows Forms and WPF";
+            // 
+            // MainTutorLbl
+            // 
+            this.MainTutorLbl.AutoSize = true;
+            this.MainTutorLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.MainTutorLbl.Location = new System.Drawing.Point(38, 117);
+            this.MainTutorLbl.Name = "MainTutorLbl";
+            this.MainTutorLbl.Size = new System.Drawing.Size(84, 20);
+            this.MainTutorLbl.TabIndex = 3;
+            this.MainTutorLbl.Text = "Main Tutor";
+            // 
+            // AlternateTutorLbl
+            // 
+            this.AlternateTutorLbl.AutoSize = true;
+            this.AlternateTutorLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.AlternateTutorLbl.Location = new System.Drawing.Point(170, 117);
+            this.AlternateTutorLbl.Name = "AlternateTutorLbl";
+            this.AlternateTutorLbl.Size = new System.Drawing.Size(115, 20);
+            this.AlternateTutorLbl.TabIndex = 4;
+            this.AlternateTutorLbl.Text = "Alternate Tutor";
+            // 
+            // SubjectLbl
+            // 
+            this.SubjectLbl.AutoSize = true;
+            this.SubjectLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.SubjectLbl.Location = new System.Drawing.Point(444, 117);
+            this.SubjectLbl.Name = "SubjectLbl";
+            this.SubjectLbl.Size = new System.Drawing.Size(100, 29);
+            this.SubjectLbl.TabIndex = 5;
+            this.SubjectLbl.Text = "Subject:";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.Animated = true;
+            this.guna2TextBox1.AutoRoundedCorners = true;
+            this.guna2TextBox1.BorderRadius = 13;
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(550, 117);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "subject in lesson";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(200, 29);
+            this.guna2TextBox1.TabIndex = 6;
+            // 
+            // MainTutorRbtn
+            // 
+            this.MainTutorRbtn.CheckedState.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.MainTutorRbtn.CheckedState.BorderThickness = 0;
+            this.MainTutorRbtn.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.MainTutorRbtn.CheckedState.InnerColor = System.Drawing.Color.CornflowerBlue;
+            this.MainTutorRbtn.CheckedState.Parent = this.MainTutorRbtn;
+            this.MainTutorRbtn.Location = new System.Drawing.Point(12, 117);
+            this.MainTutorRbtn.Name = "MainTutorRbtn";
+            this.MainTutorRbtn.ShadowDecoration.Parent = this.MainTutorRbtn;
+            this.MainTutorRbtn.Size = new System.Drawing.Size(20, 20);
+            this.MainTutorRbtn.TabIndex = 7;
+            this.MainTutorRbtn.UncheckedState.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.MainTutorRbtn.UncheckedState.BorderThickness = 2;
+            this.MainTutorRbtn.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.MainTutorRbtn.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            this.MainTutorRbtn.UncheckedState.Parent = this.MainTutorRbtn;
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.guna2CustomGradientPanel1.FillColor = System.Drawing.Color.Maroon;
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(0, 452);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.ShadowDecoration.Parent = this.guna2CustomGradientPanel1;
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(1158, 125);
+            this.guna2CustomGradientPanel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 577);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Controls.Add(this.MainTutorRbtn);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.SubjectLbl);
+            this.Controls.Add(this.AlternateTutorLbl);
+            this.Controls.Add(this.MainTutorLbl);
+            this.Controls.Add(this.GroupNameHolderLbl);
+            this.Controls.Add(this.AlternateTutorRbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2CustomRadioButton AlternateTutorRbtn;
+        private System.Windows.Forms.Label GroupNameHolderLbl;
+        private System.Windows.Forms.Label MainTutorLbl;
+        private System.Windows.Forms.Label AlternateTutorLbl;
+        private System.Windows.Forms.Label SubjectLbl;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2CustomRadioButton MainTutorRbtn;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+    }
+}
+
